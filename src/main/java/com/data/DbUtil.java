56d553045e5ceb1d6a15372f8c6987b87f4ad0f7
nomorechokedboy <haiduongit@protonmail.com>
2021-11-25 22:38:24 +0700

    update and delete review done

--- src/main/java/com/data/DbUtil.java
+++ src/main/java/com/data/DbUtil.java
@@ -1,5 +1,6 @@
 package com.data;
 
+import java.net.URI;
 import java.util.Properties;
 
 import org.hibernate.SessionFactory;
@@ -11,105 +12,120 @@ import org.hibernate.service.ServiceRegistry;
 import com.model.*;
 
 public class DbUtil {
-  private static SessionFactory sessionFactory;
+	private static SessionFactory sessionFactory;
 
-  public static SessionFactory getSessionFactory() {
-    if (sessionFactory == null) {
-      try {
-        Configuration configuration = new Configuration();
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			try {
+				Configuration configuration = new Configuration();
 
-        // Hibernate settings equivalent to hibernate.cfg.xml's properties
-        Properties settings = new Properties();
-        settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
-        settings.put(Environment.URL, "jdbc:mysql://localhost:3306/book_store?useSSL=false");
-        settings.put(Environment.USER, "root");
+				// Hibernate settings equivalent to hibernate.cfg.xml's properties
+				Properties settings = new Properties();
+				settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/book_store?useSSL=false");
+				settings.put(Environment.USER, "root");
 
-        settings.put(Environment.PASS, "ngocthien2306.com"); // remember to chang to your password
-        // {password,ngocthien2306.com}
+				settings.put(Environment.PASS, "ngocthien2306.com"); // remember to chang to your password
+				// {password,ngocthien2306.com}
 
-        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+				settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
 
-        settings.put("hibernate.connection.CharSet", "utf-8");
-        settings.put("hibernate.connection.useUnicode", true);
-        settings.put("hibernate.connection.characterEncoding", "utf-8");
+				settings.put("hibernate.connection.CharSet", "utf-8");
+				settings.put("hibernate.connection.useUnicode", true);
+				settings.put("hibernate.connection.characterEncoding", "utf-8");
 
-        settings.put(Environment.SHOW_SQL, "true");
+				settings.put(Environment.SHOW_SQL, "true");
 
-        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
-        settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+				// settings.put(Environment.HBM2DDL_AUTO, "create-drop");
 
-        // settings.put(Environment.HBM2DDL_AUTO, "update");
+				// settings.put(Environment.HBM2DDL_AUTO, "update");
 
-        configuration.setProperties(settings);
-        configuration.addAnnotatedClass(User.class);
-        configuration.addAnnotatedClass(Role.class);
-        configuration.addAnnotatedClass(Category.class);
-        configuration.addAnnotatedClass(Product.class);
-        configuration.addAnnotatedClass(Order.class);
-        configuration.addAnnotatedClass(Item.class);
-        configuration.addAnnotatedClass(Cart.class);
-        configuration.addAnnotatedClass(LineItem.class);
-        configuration.addAnnotatedClass(Review.class);
+				configuration.setProperties(settings);
+				configuration.addAnnotatedClass(User.class);
+				configuration.addAnnotatedClass(Role.class);
+				configuration.addAnnotatedClass(Category.class);
+				configuration.addAnnotatedClass(Product.class);
+				configuration.addAnnotatedClass(Order.class);
+				configuration.addAnnotatedClass(Item.class);
+				configuration.addAnnotatedClass(Cart.class);
+				configuration.addAnnotatedClass(LineItem.class);
+				configuration.addAnnotatedClass(Review.class);
 
-        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-            .applySettings(configuration.getProperties()).build();
-        System.out.println("Hibernate Java Config serviceRegistry created");
-        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-        return sessionFactory;
+				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+						.applySettings(configuration.getProperties()).build();
+				System.out.println("Hibernate Java Config serviceRegistry created");
+				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+				return sessionFactory;
 
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }
-    return sessionFactory;
-  }
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return sessionFactory;
+	}
 
-  public static SessionFactory getSessionFactors() {
-    if (sessionFactory == null) {
-      try {
-        Configuration configuration = new Configuration();
+	// Postgres
+	public static SessionFactory getSessionFactorys() {
+		if (sessionFactory == null) {
+			try {
+				Configuration configuration = new Configuration();
 
-        // Hibernate settings equivalent to hibernate.cfg.xml's properties
-        Properties settings = new Properties();
-        settings.put(Environment.DRIVER, "org.postgresql.Driver");
-        settings.put(Environment.URL,
-            "jdbc:postgresql://ec2-44-194-225-27.compute-1.amazonaws.com:5432/dbpncaer12ig4p");
-        settings.put(Environment.USER, "tmchqrkqisyfqw");
-        settings.put(Environment.PASS, "6cbad36d7efbdf936d6dfc94841fc17c1f518782d15ab48cfff785f24976d9c6");
-        settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+				Properties settings = new Properties();
 
-        settings.put(Environment.SHOW_SQL, "true");
+				String DATABASE_URL = System.getenv("DATABASE_URL");
 
-        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+				if (DATABASE_URL != null) {
+					URI dbUri = new URI(DATABASE_URL);
+					String username = dbUri.getUserInfo().split(":")[0];
+					String password = dbUri.getUserInfo().split(":")[1];
+					String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+					settings.put(Environment.URL, dbUrl);
+					settings.put(Environment.USER, username);
+					settings.put(Environment.PASS, password);
+					settings.put(Environment.DRIVER, "org.postgresql.Driver");
+					settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQL9Dialect");
+				} else {
+					settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+					settings.put(Environment.URL,
+							"jdbc:mysql://localhost:3306/book_store?allowPublicKeyRetrieval=true&useSSL=false");
+					settings.put(Environment.USER, "root");
+					settings.put(Environment.PASS, "ngocthien2306.com");
+					settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+				}
 
-        settings.put(Environment.HBM2DDL_AUTO, "update");
-        // settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+				settings.put(Environment.SHOW_SQL, "true");
+				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
-        configuration.setProperties(settings);
-        configuration.addAnnotatedClass(User.class);
-        configuration.addAnnotatedClass(Role.class);
-        configuration.addAnnotatedClass(Category.class);
-        configuration.addAnnotatedClass(Product.class);
-        configuration.addAnnotatedClass(Order.class);
-        configuration.addAnnotatedClass(Item.class);
-        configuration.addAnnotatedClass(Cart.class);
-        configuration.addAnnotatedClass(LineItem.class);
+				// settings.put(Environment.HBM2DDL_AUTO, "create-drop");
 
-        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-            .applySettings(configuration.getProperties()).build();
-        System.out.println("Hibernate Java Config serviceRegistry created");
-        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-        return sessionFactory;
+				settings.put(Environment.HBM2DDL_AUTO, "update");
 
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }
-    return sessionFactory;
-  }
+				configuration.setProperties(settings);
+				configuration.addAnnotatedClass(User.class);
+				configuration.addAnnotatedClass(Role.class);
+				configuration.addAnnotatedClass(Category.class);
+				configuration.addAnnotatedClass(Product.class);
+				configuration.addAnnotatedClass(Order.class);
+				configuration.addAnnotatedClass(Item.class);
+				configuration.addAnnotatedClass(Cart.class);
+				configuration.addAnnotatedClass(LineItem.class);
 
-  public static void shutdown() {
-    getSessionFactory().close();
-  }
+				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+						.applySettings(configuration.getProperties()).build();
+				System.out.println("Hibernate Java Config serviceRegistry created");
+				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+				return sessionFactory;
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return sessionFactory;
+	}
+
+	public static void shutdown() {
+		getSessionFactory().close();
+	}
 }
