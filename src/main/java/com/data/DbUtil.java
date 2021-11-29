06fd1e8c4ccb75eb64ea850f72df6b6a8c9ba75c
nomorechokedboy <haiduongit@protonmail.com>
2021-11-29 15:34:37 +0700

    change dbutil

--- src/main/java/com/data/DbUtil.java
+++ src/main/java/com/data/DbUtil.java
@@ -1,6 +1,5 @@
 package com.data;
 
-import java.net.URI;
 import java.util.Properties;
 
 import org.hibernate.SessionFactory;
@@ -12,9 +11,66 @@ import org.hibernate.service.ServiceRegistry;
 import com.model.*;
 
 public class DbUtil {
+
 	private static SessionFactory sessionFactory;
 
+	public static SessionFactory getSessionFactory() {
+
+		if (sessionFactory == null) {
+			try {
+				Configuration configuration = new Configuration();
+
+				// Hibernate settings equivalent to hibernate.cfg.xml's properties
+				Properties settings = new Properties();
+				settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/book_store?useSSL=false");
+				settings.put(Environment.USER, "root");
+
+				settings.put(Environment.PASS, "ngocthien2306.com"); // remember to chang to your password
+				// {password,ngocthien2306.com}
+
+				settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+				settings.put("hibernate.connection.CharSet", "utf-8");
+				settings.put("hibernate.connection.useUnicode", true);
+				settings.put("hibernate.connection.characterEncoding", "utf-8");
+
+				settings.put(Environment.SHOW_SQL, "true");
+
+				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+				// settings.put(Environment.HBM2DDL_AUTO, "update");
+				settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+				configuration.setProperties(settings);
+				configuration.addAnnotatedClass(User.class);
+				configuration.addAnnotatedClass(Role.class);
+				configuration.addAnnotatedClass(Category.class);
+				configuration.addAnnotatedClass(Product.class);
+				configuration.addAnnotatedClass(Order.class);
+				configuration.addAnnotatedClass(Item.class);
+				configuration.addAnnotatedClass(Cart.class);
+				configuration.addAnnotatedClass(LineItem.class);
+				configuration.addAnnotatedClass(Review.class);
+				configuration.addAnnotatedClass(Promo.class);
+				configuration.addAnnotatedClass(File.class);
+				configuration.addAnnotatedClass(Photo.class);
+
+				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+						.applySettings(configuration.getProperties()).build();
+				System.out.println("Hibernate Java Config serviceRegistry created");
+				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+				return sessionFactory;
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return sessionFactory;
+	}
+
 	public static SessionFactory getSessionFactorys() {
+
 		if (sessionFactory == null) {
 			try {
 				Configuration configuration = new Configuration();
@@ -30,7 +86,7 @@ public class DbUtil {
 
 				settings.put(Environment.SHOW_SQL, "true");
 
-				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+				// settings.put(Environment.HBM2DDL_AUTO, "create-drop");
 
 				settings.put(Environment.HBM2DDL_AUTO, "update");
 				// settings.put(Environment.HBM2DDL_AUTO, "create-drop");
@@ -44,65 +100,10 @@ public class DbUtil {
 				configuration.addAnnotatedClass(Item.class);
 				configuration.addAnnotatedClass(Cart.class);
 				configuration.addAnnotatedClass(LineItem.class);
-
-				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-						.applySettings(configuration.getProperties()).build();
-				System.out.println("Hibernate Java Config serviceRegistry created");
-				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-				return sessionFactory;
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return sessionFactory;
-
-	}
-
-	public static SessionFactory getSessionFactory() {
-		if (sessionFactory == null) {
-			try {
-				Configuration configuration = new Configuration();
-
-				Properties settings = new Properties();
-
-				String DATABASE_URL = System.getenv("DATABASE_URL");
-
-				if (DATABASE_URL != null) {
-					URI dbUri = new URI(DATABASE_URL);
-					String username = dbUri.getUserInfo().split(":")[0];
-					String password = dbUri.getUserInfo().split(":")[1];
-					String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-					settings.put(Environment.URL, dbUrl);
-					settings.put(Environment.USER, username);
-					settings.put(Environment.PASS, password);
-					settings.put(Environment.DRIVER, "org.postgresql.Driver");
-					settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQL9Dialect");
-				} else {
-					settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
-					settings.put(Environment.URL,
-							"jdbc:mysql://localhost:3306/book_store?allowPublicKeyRetrieval=true&useSSL=false");
-					settings.put(Environment.USER, "root");
-					settings.put(Environment.PASS, "ngocthien2306.com");
-					settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-				}
-
-				settings.put(Environment.SHOW_SQL, "true");
-				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-
-				// settings.put(Environment.HBM2DDL_AUTO, "create-drop");
-
-				settings.put(Environment.HBM2DDL_AUTO, "update");
-
-				configuration.setProperties(settings);
-				configuration.addAnnotatedClass(User.class);
-				configuration.addAnnotatedClass(Role.class);
-				configuration.addAnnotatedClass(Category.class);
-				configuration.addAnnotatedClass(Product.class);
-				configuration.addAnnotatedClass(Order.class);
-				configuration.addAnnotatedClass(Item.class);
-				configuration.addAnnotatedClass(Cart.class);
-				configuration.addAnnotatedClass(LineItem.class);
+				configuration.addAnnotatedClass(Review.class);
+				configuration.addAnnotatedClass(Promo.class);
+				configuration.addAnnotatedClass(File.class);
+				configuration.addAnnotatedClass(Photo.class);
 
 				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
 						.applySettings(configuration.getProperties()).build();
@@ -120,4 +121,5 @@ public class DbUtil {
 	public static void shutdown() {
 		getSessionFactory().close();
 	}
+
 }
